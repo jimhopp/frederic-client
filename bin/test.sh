71a856fecf73e4af7b9f9ed1f559f905eb722fa0
Jim Hopp <jimhopp@gmail.com>
2016-12-24 13:19:04 -0800

    add a bit more info on bb5224aed727ef11e89a826bab2b8dc43bb5e71a, see also #1

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+#had to hard-code the id because of travis-ci/travis-ci#7031
 #xcodebuild test -scheme frederic-client -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2'
 xcodebuild test -scheme frederic-client -destination 'id=1FD54EA7-5A25-4D6F-8599-D6F7687DA4EE'
