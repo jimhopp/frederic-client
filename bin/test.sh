bb5224aed727ef11e89a826bab2b8dc43bb5e71a
Jim Hopp <jimhopp@gmail.com>
2016-12-24 13:03:41 -0800

    Apparently a problem with the travis-ci xcode images  requires specifying an id

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-xcodebuild test -scheme frederic-client -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2'
+#xcodebuild test -scheme frederic-client -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2'
+xcodebuild test -scheme frederic-client -destination 'id=1FD54EA7-5A25-4D6F-8599-D6F7687DA4EE'
