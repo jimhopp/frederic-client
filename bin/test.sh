3ba3412826b5766c8341b503ff1c44b8e68a6f0f
Jim Hopp <jimhopp@gmail.com>
2016-12-24 10:00:51 -0800

    adding id to the test destination, not sure why it is needed

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xcodebuild test -scheme frederic-client -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2'
+xcodebuild test -scheme frederic-client -destination 'platform=iOS Simulator,id=AD8E8C76-910E-437D-88C2-4D6F6EBE3355,name=iPhone 6,OS=10.2'
