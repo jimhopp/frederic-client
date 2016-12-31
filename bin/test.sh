681c3317cc52bff28a68c5c08a3414cd225bca6d
Jim Hopp <jimhopp@gmail.com>
2016-12-31 14:19:25 -0800

    make pod update run before travis install

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-pod repo update
-pod install
+before_install:
+- pod repo update
 
+install:
+- pod install
+
+script:
 #had to hard-code the id because of travis-ci/travis-ci#7031
 #xcodebuild test -workspace frederic-client.xcworkspace/ -scheme frederic-client -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2'
-xcodebuild test -workspace frederic-client.xcworkspace/ -scheme frederic-client -destination 'id=1FD54EA7-5A25-4D6F-8599-D6F7687DA4EE'
+- xcodebuild test -workspace frederic-client.xcworkspace/ -scheme frederic-client -destination 'id=1FD54EA7-5A25-4D6F-8599-D6F7687DA4EE'
