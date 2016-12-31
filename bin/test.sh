cd3fb46f80993ed59b3c2a863a3fcc165f1a656d
Jim Hopp <jimhopp@gmail.com>
2016-12-31 14:31:49 -0800

    Revert "make pod update run before travis install"
    This reverts commit 681c3317cc52bff28a68c5c08a3414cd225bca6d.

--- bin/test.sh
+++ bin/test.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-before_install:
-- pod repo update
+pod repo update
+pod install
 
-install:
-- pod install
-
-script:
 #had to hard-code the id because of travis-ci/travis-ci#7031
 #xcodebuild test -workspace frederic-client.xcworkspace/ -scheme frederic-client -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2'
-- xcodebuild test -workspace frederic-client.xcworkspace/ -scheme frederic-client -destination 'id=1FD54EA7-5A25-4D6F-8599-D6F7687DA4EE'
+xcodebuild test -workspace frederic-client.xcworkspace/ -scheme frederic-client -destination 'id=1FD54EA7-5A25-4D6F-8599-D6F7687DA4EE'
