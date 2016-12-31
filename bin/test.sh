61a51f4d64463c449721656aeb1e09bb02f65447
Jim Hopp <jimhopp@gmail.com>
2016-12-31 14:24:59 -0800

    more debugging on travis build

--- bin/test.sh
+++ bin/test.sh
@@ -9,4 +9,6 @@ install:
 script:
 #had to hard-code the id because of travis-ci/travis-ci#7031
 #xcodebuild test -workspace frederic-client.xcworkspace/ -scheme frederic-client -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2'
+- pod env
+- pod repo list
 - xcodebuild test -workspace frederic-client.xcworkspace/ -scheme frederic-client -destination 'id=1FD54EA7-5A25-4D6F-8599-D6F7687DA4EE'
